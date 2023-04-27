4fda5fc65f63e58712b460b65464355114cc00ac
geedtd <grcazares@gmail.com>
2023-04-27 15:27:30 -0700

    marker showing up on google maps

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,19 @@
-
-
 import './App.css'
-import { useLoadScript, GoogleMap, Marker } from '@react-google-maps/api';
+import { useLoadScript, GoogleMap, Marker} from '@react-google-maps/api';
 
 function App() {
   const { isLoaded } = useLoadScript({
     googleMapsApiKey: import.meta.env.VITE_API_KEY,
   })
-  console.log(import.meta.VITE_API_KEY)
+  // console.log(import.meta.VITE_API_KEY)
 
   if (!isLoaded) return <div>Loading...</div>
-  return (
-    <>
-      <div>MAP</div>
-    </>
-  )
+  return <GMap />
 }
 
+function GMap() {
+  return <GoogleMap zoom={10} center={{lat: 44, lng: -80}} mapContainerClassName='map-container' >
+    <Marker position={{lat:44, lng: -80}} />
+  </GoogleMap>
+}
 export default App
